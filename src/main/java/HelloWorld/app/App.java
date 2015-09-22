e15d25256a93b594de8cf1cf3d33b99f6ce94e1d
Maros Staurovsky <xstaurov@nymfe41.fi.muni.cz>
2015-09-22 19:03:44 +0200

    First commit

---
+++ src/main/java/HelloWorld/app/App.java
@@ -0,0 +1,15 @@
+package HelloWorld.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	System.out.println("");
+        System.out.println( "Hello PA165!" );
+	System.out.println("");
+    }
+}
